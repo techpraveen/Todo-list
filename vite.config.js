0f460a48040f0d4b3bcf3e37a971fcf06cba7a39
Praveen Kumar Pandey <95954302+techpraveen@users.noreply.github.com>
2025-02-16 01:59:42 +0530

    Update vite.config.js
    another changes made

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
+ 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Render serves static sites from 'dist'
-    assetsDir: 'assets', // Stores static assets like CSS, images, JS
-    emptyOutDir: true, // Clears the folder before each build
-  },
-  server: {
-    port: 3000, // Ensure Vite runs on port 3000 (Render supports it)
+    outDir: 'dist', // Output directory for the production build
   },
 });
